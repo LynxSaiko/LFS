442860ed4601cd1fe9fea8f7c63b56a8bcef7ff2
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-02 04:12:06 +0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-# download-lang.sh - Unduh source Python 2.7.18, Ruby 2.7.8, dan Ruby 3.3.0
-# Author: Lynx
+# Author: LynxSaiko
 # Date: $(date +%F)
 
 set -e
-
-# Python 2.7.18
-echo "[+] Downloading Python 2.7.18..."
-wget -c https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tar.xz
-
-# Ruby 2.7.8
-echo "[+] Downloading Ruby 2.7.8..."
-wget -c https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.8.tar.gz
-
-# Ruby 3.3.0
-echo "[+] Downloading Ruby 3.3.0..."
-wget -c https://cache.ruby-lang.org/pub/ruby/3.3/ruby-3.3.0.tar.gz
-
-echo "[✔] Semua source berhasil diunduh ke /sources"
+sudo apt update
+sudo apt install -y \
+    build-essential flex bison gawk texinfo wget \
+    python3 tar git gzip bzip2 xz-utils \
+    m4 make patch gcc g++ libc6-dev libc6-dev \
+    libgmp-dev libmpfr-dev libmpc-dev libncurse-dev
