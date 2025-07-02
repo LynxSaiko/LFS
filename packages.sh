e0e472f1cc213dc71afc45c0322b7178c1277c4c
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-02 11:29:30 +0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -8,4 +8,4 @@ sudo apt install -y \
     build-essential flex bison gawk texinfo wget \
     python3 tar git gzip bzip2 xz-utils \
     m4 make patch gcc g++ libc6-dev libc6-dev \
-    libgmp-dev libmpfr-dev libmpc-dev libncurse-dev
+    libgmp-dev libmpfr-dev libmpc-dev libncurses-dev
