25f4e5473d9238ab534359f9b540220b6e17aba2
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-02 12:08:39 +0700

    Update install-chroot.sh

--- install-chroot.sh
+++ install-chroot.sh
@@ -54,67 +54,14 @@ finish
 
 # 7.10. Python-3.10.6
 begin Python-3.10.6 tar.xz
-./configure --prefix=/usr            \
-            --enable-shared          \
-            --with-system-expat      \
-            --with-system-ffi        \
-            --enable-optimizations
-
-make -j$(nproc)
-make install
-# pip config (optional)
-mkdir -pv /etc
-cat > /etc/pip.conf << EOF
-[global]
-disable-pip-version-check = true
-root-user-action = ignore
-EOF
-# Symlink eksplisit
-ln -sfv /usr/bin/python3
-ln -sfv /usr/bin/pip3
-finish
-
-
-echo "[python2] Configuring..."
-begin Python-2.7.18.tar.xz
-./configure --prefix=/usr              \
-            --enable-shared            \
-            --with-system-expat        \
-            --with-system-ffi          \
-            --enable-unicode=ucs4      \
-            --with-ensurepip=yes
-
-make -j$(nproc)
-make install
-ln -sfv /usr/bin/python2
-ln -sfv /usr/bin/pip2
-finish
-
-echo "[ruby2] Configuring..."
-begin ruby-2.7.8.tar.gz
-./configure --prefix=/usr --program-suffix=2.7 --disable-install-doc
-make -j$(nproc)
+./configure --prefix=/usr   \
+            --enable-shared \
+            --without-ensurepip
+make
 make install
 finish
 
-echo "[ruby3] Configuring..."
-begin ruby-3.3.0.tar.gz
-./configure --prefix=/usr --program-suffix=3.3 --disable-install-doc
-make -j$(nproc)
-make install
-finish
-
-
-# Symlink untuk ruby2 & ruby3
-ln -sfv /usr/bin/ruby2.7 /usr/bin/ruby2
-ln -sfv /usr/bin/gem2.7  /usr/bin/gem2
-ln -sfv /usr/bin/irb2.7  /usr/bin/irb2
-
-ln -sfv /usr/bin/ruby3.3 /usr/bin/ruby3
-ln -sfv /usr/bin/gem3.3  /usr/bin/gem3
-ln -sfv /usr/bin/irb3.3  /usr/bin/irb3
 # 7.11. Texinfo-6.8
-
 echo "texinfo"
 begin texinfo-6.8 tar.xz
 ./configure --prefix=/usr
