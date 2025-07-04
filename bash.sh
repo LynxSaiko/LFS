7f2e102f8ea9723ff7dd4a3ec38634c0fb5572fa
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-04 18:34:09 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LFS=/mnt/lfs
+ROOTDIR=$LFS/root
 
 cat > ~/.bashrc << "EOF"
 set +h
@@ -18,3 +20,19 @@ exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 EOF
 
 source ~/.bashrc
+
+
+echo "[+] Membuat /root/.bash_profile..."
+cat > $ROOTDIR/.bash_profile << "EOF"
+exec env -i HOME=/root TERM=$TERM PS1='(lfs) \u:\w\$ ' /bin/bash --login
+EOF
+
+echo "[+] Membuat /root/.bashrc..."
+cat > $ROOTDIR/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+export LFS LC_ALL LFS_TGT PATH MAKEFLAGS
+EOF
