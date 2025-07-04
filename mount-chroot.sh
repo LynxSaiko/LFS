884a5d76d85d2ea206a70dadd367c1d082a62f3a
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-04 18:30:45 +0700

    Update mount-chroot.sh

--- mount-chroot.sh
+++ mount-chroot.sh
@@ -13,5 +13,5 @@ chroot "$LFS" /usr/bin/env -i   \
     HOME=/root                  \
     TERM="$TERM"                \
     PS1='(lfs chroot) \u:\w\$ ' \
-    PATH=/bin:/usr/bin:/usr/sbin     \
-    /bin/bash --login
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login +h
