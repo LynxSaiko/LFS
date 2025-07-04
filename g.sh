2a3bf644f52e2e201ba103e413d30e5c243b33a8
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-05 04:50:56 +0700

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,7 +1,8 @@
 cat > /boot/grub/grub.cfg << "EOF"
 set default=0
 set timeout=5
-
+insmod ext2
+set root=(hd0,2)
 menuentry "LeakOS LFS 5.19.2" {
     linux /boot/vmlinuz-leakos root=/dev/sda1 ro
 }
