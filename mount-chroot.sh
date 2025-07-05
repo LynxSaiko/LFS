41a673f4fe453a68d22f4ae4661089221b80b2fe
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-06 02:13:44 +0700

    Update mount-chroot.sh

--- mount-chroot.sh
+++ mount-chroot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-mknod -m 600 $LFS/dev/console c 5 1
-mknod -m 666 $LFS/dev/null c 1 3
+mknod -m 600 $LFS/dev/console c 5 1 || true
+mknod -m 666 $LFS/dev/null c 1 3 || true
+mknod -m 666 $LFS/dev/zero c 1 5 || true
+mknod -m 622 $LFS/dev/tty c 5 0 || true
+chown root:tty $LFS/dev/console $LFS/dev/tty || true
+
 mount -v --bind /dev $LFS/dev
 mount -v --bind /dev/pts $LFS/dev/pts
 mount -vt proc proc $LFS/proc
