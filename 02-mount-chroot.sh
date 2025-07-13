50323bc8bab0431014ca497c7482d087ca8d9347
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-14 01:51:35 +0700

    Update 02-mount-chroot.sh

--- 02-mount-chroot.sh
+++ 02-mount-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir -pv $LFS/{dev,proc,sys,run}
 mount -v --bind /dev $LFS/dev
 mount -v --bind /dev/pts $LFS/dev/pts
 mount -vt proc proc $LFS/proc
@@ -8,6 +8,7 @@ mount -vt tmpfs tmpfs $LFS/run
 if [ -h $LFS/dev/shm ]; then
   mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 fi
+
 chroot "$LFS" /usr/bin/env -i   \
     HOME=/root                  \
     TERM="$TERM"                \
