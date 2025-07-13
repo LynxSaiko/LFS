1d20ad2a39a2b429f96baf78d32023d241119e17
RedTeamingSec <redteamsectorid@gmail.com>
2025-07-13 23:59:33 +0700

    Update 02-mount-chroot.sh

--- 02-mount-chroot.sh
+++ 02-mount-chroot.sh
@@ -3,7 +3,6 @@ mknod -m 600 $LFS/dev/console c 5 1 || true
 mknod -m 666 $LFS/dev/null c 1 3 || true
 mknod -m 666 $LFS/dev/zero c 1 5 || true
 mknod -m 622 $LFS/dev/tty c 5 0 || true
-chown root:tty $LFS/dev/console $LFS/dev/tty || true
 
 mount -v --bind /dev $LFS/dev
 mount -v --bind /dev/pts $LFS/dev/pts
@@ -18,4 +17,4 @@ chroot "$LFS" /usr/bin/env -i   \
     TERM="$TERM"                \
     PS1='(lfs chroot) \u:\w\$ ' \
     PATH=/usr/bin:/usr/sbin     \
-    /bin/bash --login +h
+    /bin/bash --login
