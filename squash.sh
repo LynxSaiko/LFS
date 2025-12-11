776535ffe5b592e4ac82e725b2e03ed4b4edb951
LynxSaiko <redteamsectorid@gmail.com>
2025-12-12 02:12:31 +0700

    Update squash.sh

--- squash.sh
+++ squash.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Jalankan sebagai root (atau pakai sudo)
-sudo mksquashfs / live/boot/filesystem.sfs \
-    -comp gzip -Xcompression-level 9 -b 1M \
-    -no-xattrs -noappend \
-    -wildcards \
-    -e dev/* proc/* sys/* run/* tmp/* \
-       root/* tools/* lost+found var/* mnt/* media/* \
-       boot/vmlinuz* boot/initrd* boot/System.map* boot/config* \
-       \
-       .cache npm .npm node_modules .git .nvm .yarn \
-       .node-gyp pip_cache \
-       home/*/.cache home/*/.npm home/*/.node-gyp \
-       home/*/.nvm home/*/node_modules home/*/.git \
-       home/*/.yarn home/*/.pip_cache \
-       root/.cache root/.npm root/node_modules root/.git
+mksquashfs /mnt/lfs live/boot/filesystem.sfs \
+    -b 1048576 \
+    -comp gzip \
+    -e /root/* \
+    -e /tmp/* \
+    -e /dev/* \
+    -e /proc/* \
+    -e /sys/* \
+    -e /run/* \
+    -e /home/*\
+    -e /var/* \
+    -e /usr/share/doc/* \
+    -e /usr/share/info* \
+    -e /mnt/* \
+    -e /media/*
